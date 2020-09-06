7535c6be0a2f407d15129d960eae0f8447b5eef4
Adrian Marquis <adrian.marquis78@gmail.com>
2020-09-06 12:42:27 -0400

    refactored with heroku

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const helmet = require('helmet')
 const MOVIES = require('./moviedex.json')
 
 const app = express()
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morgan('dev'))
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
-    console.log('validate bearer token middleware')
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')   
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
@@ -45,7 +45,17 @@ app.get('/movie', function handleGetMovie(req, res) {
     res.json(response)
   })  
 
-const PORT = 9000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
